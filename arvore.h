4d2ae13e2592e2e0dd94a87fae164f4775d3a1c3
D1m1tri <enzosantanna00@gmail.com>
2022-12-21 12:14:44 -0300

    starting search functions

---
+++ arvore.h
@@ -0,0 +1,106 @@
+typedef struct linha {
+	int linha;
+	struct linha * proximo;
+	int existeProximo;
+} LinhasArvore;
+
+typedef struct _no_arvore_ {
+	LinhasArvore * linhas;
+	char * palavra;
+	int aparicoes;
+	struct _no_arvore_ * esq;
+	struct _no_arvore_ * dir;
+} No;
+
+typedef struct {
+	No * raiz;
+} Arvore;
+
+
+
+
+Arvore * cria_arvore(){				// cria a base da Arvore
+	Arvore * arvore = (Arvore *) malloc (sizeof(Arvore));
+	arvore->raiz = (No *) malloc(sizeof(No));	
+	return arvore;
+}
+
+
+
+
+No * busca_bin_rec(No * no, char* palavra){	// busca por uma palavra (parte recursiva)
+
+	if(no){
+
+		if(!strcmp(no->palavra, palavra)) return no;
+		if(strcmp(no->palavra, palavra) < 0) return busca_bin_rec(no->esq, palavra);
+		return busca_bin_rec(no->dir, palavra);
+	}
+
+	return NULL;
+}
+No * busca_bin(Arvore * arvore, char* palavra){	// busca por uma palavra (parte a ser chamada)
+	
+	return busca_bin_rec(arvore->raiz, palavra);	
+}
+
+
+
+No * criarNo(){					// cria um novo Nó e retorna seu ponteiro
+	No * novo = (No *)malloc(sizeof(No));
+	novo->esq = novo->dir = NULL;
+	novo->linhas = (LinhasArvore *) malloc(sizeof(LinhasArvore));
+	novo->aparicoes = 0;
+	return novo;
+}
+
+void insertLineArvore(LinhasArvore * linhaAtual, int lineNum){
+	int noRepeat = 1;
+	while(linhaAtual->existeProximo == 1){
+		linhaAtual = linhaAtual->proximo;
+		if(linhaAtual->linha == lineNum){
+			noRepeat = 0;
+		}
+	}
+	if(noRepeat){
+		linhaAtual->existeProximo = 1;
+		linhaAtual->proximo = (LinhasArvore *) malloc(sizeof(LinhasArvore));
+		linhaAtual = linhaAtual->proximo;
+		linhaAtual->linha = lineNum;
+		linhaAtual->existeProximo = 0;
+	}
+}
+
+void insere_ord_rec(No * raiz, char * palavra, int linha){// insere uma palavra nova (recursão)
+
+	if(strcmp(raiz->palavra, palavra)){
+		if(strcmp(raiz->palavra, palavra) < 0){
+			if(raiz->esq){
+				insere_ord_rec(raiz->esq, palavra, linha);
+			}
+			else{ 
+				raiz->esq = criarNo();
+				raiz = raiz->esq;
+			}
+		}
+		else{
+			if(raiz->dir){
+				insere_ord_rec(raiz->dir, palavra, linha);
+			}
+			else{
+				raiz->dir = criarNo();
+				raiz=raiz->dir;
+			}
+		}
+		strcpy(raiz->palavra, palavra);
+	}
+	if(!raiz->palavra){
+		strcpy(raiz->palavra, palavra);
+	}
+	insertLineArvore(raiz->linhas, linha);
+	raiz->aparicoes++;
+}
+
+void insere_ord(Arvore * arvore, char* palavra, int linha){// insere uma palavra nova (chamar)
+	insere_ord_rec(arvore->raiz, palavra, linha);
+}
---
+++ lista.h
@@ -0,0 +1,85 @@
+typedef struct linhas {
+	int linha;
+	struct linhas * proximo;
+	int existeProximo;
+} Linhas;
+
+typedef struct lista {
+	int numLinhas;
+	Linhas * linhas;
+	char * palavra;
+	struct lista * proximo;
+	int existeProximo;
+} Lista;
+
+typedef struct {
+	Lista * lista;
+} Inicio;
+
+void ini(Inicio * atual){
+	atual->lista = (Lista*) malloc(sizeof(Lista));
+}
+
+void createList(Lista * atual, Lista * substituto){	//cria um Nó novo e zera os valores
+	atual->proximo = (Lista *) malloc(sizeof(Lista));
+	atual->existeProximo = 1;
+	atual = atual->proximo;
+	atual->linhas = (Linhas *) malloc(sizeof(Linhas));
+	atual->linhas->existeProximo = 0;
+	atual->existeProximo=0;
+	if(substituto){
+		atual->proximo = substituto;
+		atual->existeProximo=1;
+	}
+	atual->numLinhas = 0;
+}
+
+void insertLine(Linhas * linhaAtual, int lineNum){
+	int noRepeat = 1;
+	while(linhaAtual->existeProximo == 1){
+		linhaAtual = linhaAtual->proximo;
+		if(linhaAtual->linha == lineNum){
+			noRepeat = 0;
+		}
+	}
+	if(noRepeat){
+		linhaAtual->existeProximo = 1;
+		linhaAtual->proximo = (Linhas *) malloc(sizeof(Linhas));
+		linhaAtual = linhaAtual->proximo;
+		linhaAtual->linha = lineNum;
+		linhaAtual->existeProximo = 0;
+	}
+}
+
+void InsertOnList(Lista * atual, char palavra[], int lineNum){
+	int doit = 1;
+	while(doit){
+		if(atual->existeProximo == 0){
+			createList(atual, NULL);
+			doit = 0;
+		}else{
+		
+		if(strcmp(atual->proximo->palavra, palavra) > 0){
+			createList(atual, atual->proximo);
+			doit = 0;
+		}else{
+
+		if(strcmp(atual->proximo->palavra, palavra) == 0){
+			doit = 0;
+		}else{
+
+		atual = atual->proximo;
+		}}}
+	}
+	int size = strlen(palavra);
+	size++;
+	atual = atual->proximo;
+	if(atual->palavra == NULL){
+		atual->palavra = (char *) malloc(size*sizeof(char));
+		for(int i=0; i<size; i++){
+			atual->palavra[i] = palavra[i];
+		}
+	}
+	atual->numLinhas++;
+	insertLine(atual->linhas, lineNum);
+}
